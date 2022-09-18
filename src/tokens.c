1e11c6f3e2c86fd606d1f5420af7b1fe4b3af24f
bmen2a <bmendoza7@miners.utep.edu>
2022-09-18 13:55:39 -0600

    Finish last 3 methods

--- src/tokens.c
+++ src/tokens.c
@@ -25,10 +25,17 @@ char *word_start(char *str){
   return str;
 }
   char *word_terminator(char *word){
-    while( *word == '\0' && isspace(*word){
-	word++;
+    word=word_start(word);
+    //check if its at the end already
+    if(word==NULL)
+      return NULL;
+    int i=0;
+    while( non_space_char(word[i])==1){
+      if(word[i]=='\0')
+	return &word[i-1];
+      i++;
       }
-      return word;
+      return &word[i];
   }
     int count_words(char *str){
       int counter;
@@ -39,5 +46,37 @@ char *word_start(char *str){
       return counter;
     }
     char *copy_str(char *inStr, short len){
-      *inStr= (char*) malloc(len*sizeof(int));
+      int i=0;
+      char *outStr= malloc((len+1)*sizeof(int));
+      if(outStr==NULL){
+	fprintf(stderr, "Error\n");
+	return NULL;
+      }//wihle loop to copy string
+      while(i<len){
+	outStr[i]=inStr[i];
+	i++;
+      }
+      outStr[i]="\0"; //set the end of String
+      return outStr;
     }
+    char **tokenizer (char* str){
+      int i=0;
+      int all=count_words(str);
+      char **tokens=malloc(all+1)*sizeof(char*));
+    if(tokens==NULL){
+      fprintf(stderr, "error: allocating memory\n");
+      i++;
+    }
+    tokens[i]=NULL;
+    return tokens;
+    }
+void print_tokens(char **tokens){
+  
+  while(tokens!=NULL){
+    printf("%s\n", tokens);
+    tokens++;
+  }
+}
+void free_tokens(char **tokens){
+  free(tokens);// the free method releases the memory at that address
+}
